9fbb17e1c211902985315097dafa75628a144fef
thrishul-alathanda <44458270+thrishul-alathanda@users.noreply.github.com>
2021-08-14 01:09:16 +0530

    day 1 react chatsapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// Firebase deps
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCzuB9ECPVb9MgXb_xYQGvFE9Uc6Ww2QCA",
+    authDomain: "react-chatsapp.firebaseapp.com",
+    projectId: "react-chatsapp",
+    storageBucket: "react-chatsapp.appspot.com",
+    messagingSenderId: "207601206642",
+    appId: "1:207601206642:web:c47c142af8b23f668e49eb",
+});
+
+const auth = firebase.auth();
 
 function App() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState(() => auth.currentUser);
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(false);
+      }
+      if (initializing) {
+        setInitializing(false);
+      }
+    });
+
+    // Cleanup subscription
+    return unsubscribe;
+  }, []);
+
+  const signInWithGoogle = async () => {
+    // Retrieve Google provider object
+    const provider = new firebase.auth.GoogleAuthProvider();
+    // Set language to the default browser preference
+    firebase.auth().useDeviceLanguage();
+    // Start sign in process
+    try {
+      await firebase.auth().signInWithPopup(provider);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  if (initializing) return "Loading...";
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user ? ('Welcome to the chat') : (<Button onClick={signInWithGoogle}>Sign in with Google</Button>) }
     </div>
   );
 }
